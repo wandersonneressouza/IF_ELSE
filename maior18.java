b608d3eb359dd608d17f58d5524c62365b8836e4
Wanderson Neres Souza <114609327+wandersonneressouza@users.noreply.github.com>
2026-02-02 21:23:15 -0300

    maior18

---
+++ maior18.java
@@ -0,0 +1,17 @@
+package JAVA.IF_LIST;
+
+public class maior18 {
+
+    public static void main(String[] args) {
+        
+        int numb;
+        numb = 18;
+
+        if (numb > 17){
+                System.out.println("VC É DE MAIOR");
+
+        }else{System.out.println("VC É DE MENOR!");}
+    }
+
+    
+}
